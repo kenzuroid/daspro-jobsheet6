3e2a17f639b4fe14730a616b3b8eb4bf1344547b
kenzuroid <sholihahsitinikmatus@gmail.com>
2024-10-02 11:33:27 +0700

    perubahan percobaan 1

--- Pemilihan2Percobaan122.java
+++ Pemilihan2Percobaan122.java
@@ -13,10 +13,9 @@ public class Pemilihan2Percobaan122 {
 
         if ((tahun % 4)== 0 ){
             if ((tahun % 100) != 0 )
-            {
                 System.out.println("Tahun Kabisat");
-            } else 
-            System.out.println("Bukan Tahun Kabisat");
+        else
+        System.out.println("Bukan Tahun Kabisat");
         }
 
     }
